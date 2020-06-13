dc99eed6b8eeb27d39748a413e44f97d241b19ba
Fabio Brito <fabio.britto15@gmail.com>
2020-06-13 12:51:43 -0300

    chore: add migrations | Order and User tables

---
+++ src/shared/infra/typeorm/migrations/1592061887319-CreateOrderTable.ts
@@ -0,0 +1,69 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateOrderTable1592061887319
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'orders',
+        columns: [
+          {
+            name: 'order_id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'value',
+            type: 'decimal',
+            precision: 2,
+            scale: 2,
+          },
+          {
+            name: 'cashback_percentage',
+            type: 'integer',
+          },
+          {
+            name: 'cashback_value',
+            type: 'decimal',
+            precision: 2,
+            scale: 2,
+          },
+          {
+            name: 'status',
+            type: 'varchar',
+          },
+          {
+            name: 'user_id',
+            type: 'uuid',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'UserReference',
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            columnNames: ['user_id'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('orders');
+  }
+}
